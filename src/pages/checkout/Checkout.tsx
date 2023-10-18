e48146d7211250c797efa8ab9d161214dce612c9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-17 22:20:06 -0300

    aula 17.10

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -1,117 +1,97 @@
-import { useContext, useEffect, useState } from "react"
-import OrderContext from "../../contexts/OrderContext"
-import { useNavigate } from "react-router-dom"
-import { Layout } from "../../components/layout/Layout"
-import { routes } from "../../routes"
-import { convertToCurrency } from "../../helpers/convertToCurrency"
-import { Title } from "../../components/title/Title"
-import { Button } from "../../components/button/Button"
+import { useContext, useEffect, useState } from "react";
+import OrderContext from "../../contexts/OrderContext";
+import { useNavigate } from "react-router-dom";
+import { Layout } from "../../components/layout/Layout";
+import { routes } from "../../routes";
+import { convertToCurrency } from "../../helpers/convertToCurrency";
+import { Title } from "../../components/title/Title";
+import { Button } from "../../components/button/Button";
 import {
   CheckoutAction,
   CheckoutItem,
   CheckoutItemFlex,
   PaymentMethodGroup,
-} from "./Checkout.style"
+} from "./Checkout.style";
 
 export default function Checkout() {
-  const { pizzaOrder } = useContext(OrderContext)
-  const navigate = useNavigate()
+  const { pizzaOrder } = useContext(OrderContext);
+  const navigate = useNavigate();
 
-  const paymentOptions = [
-    { id: "20", value: 1, text: "Cartão de crédito" },
-    { id: "21", value: 2, text: "Cartão de débito" },
-    { id: "22", value: 3, text: "Vale refeição" },
-    { id: "23", value: 4, text: "PIX" },
-  ]
+  const [paymentType, setPaymentType] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [paymentOptions, setPaymentOptions] = useState([]);
 
-  const [paymentType, setPaymentType] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+  const getPaymentOptions = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:8000/payment/options");
+      const options = await response.json();
+      setPaymentOptions(options);
+    } catch (error) {
+      alert(`Deu ruim: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const handleChange = (event) => {
-    setPaymentType(event.target.value)
-  }
+    setPaymentType(event.target.value);
+  };
 
   const getPaymentOptionType = (paymentType: number) => {
-    if (!paymentType) return
+    if (!paymentType) return;
 
     const filteredValue = paymentOptions.filter(
       (payment) => payment.value === paymentType
-    )
+    );
 
-    return filteredValue[0].value
+    return filteredValue[0].value;
+  };
+
+  const createOrder = (orderPayload) => {
+    try {
+      fetch("http://localhost:8000/payment/options", {
+        method: "POST",
+        body: JSON.stringify(orderPayload),
+      })
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("finally");
+    }
   }
 
-  // const handleClick = () => {
-  //   setIsLoading(true)
-    
-  //   fetch('https://www.api.pizzalivery.com.br/order', {
-  //     method: "POST",
-  //     body: JSON.stringify(pizzaOrder),
-  //   })
-  //     .then(() => {
-  //       alert("Deu bom")
-  //     })
-  //     .catch(() => {
-  //       console.log("Deu ruim")
-  //     })
-  //     .finally(() => {
-  //       setIsLoading(false)
-  //       console.log("Finalizou")
-  //     })
-  // }
+  const handleClick = () => {
+    createOrder(pizzaOrder)
+  }
 
   // async/await
   const doOrder = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
 
-    try{
-      const response = await fetch('https://www.api.pizzalivery.com.br/order', 
-          {
-            method: "POST",
-            body: JSON.stringify(pizzaOrder),
-          }
-        )
+    try {
+      const response = await fetch("https://www.api.pizzalivery.com.br/order", {
+        method: "POST",
+        body: JSON.stringify(pizzaOrder),
+      });
 
-        console.log(response)
+      console.log(response);
     } catch (error) {
-      alert(error)
+      alert(error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
-
-  // //  METHODS/VEBS HTTP
-  // //  GET: Receber uma informação; √
-  // fetch("https://www.api.pizzalivery.com.br/order")
-
-  // const filter = "coca cola"
-  // fetch(`https://www.api.pizzalivery.com.br/bebidas=${filter}`)
-
-  // //  POST: Enviar uma informação; √
-  // fetch("https://www.api.pizzalivery.com.br/order", {
-  //   method: "POST",
-  //   body: JSON.stringify(pizzaOrder),
-  // })
-
-  // //  PUT: Alterar uma informação; √
-  // fetch("https://www.api.pizzalivery.com.br/order", {
-  //   method: "PUT",
-  //   body: JSON.stringify(pizzaOrder),
-  // })
-
-  // //  DELETE: Remover uma informação;
-  // const orderID = 12345100;
-
-  // fetch(`https://www.api.pizzalivery.com.br/order/${orderID}`, {
-  //   method: "DELETE",
-  // })
-
+  };
 
   useEffect(() => {
     if (pizzaOrder === undefined) {
-      return navigate(routes.pizzaSize)
+      return navigate(routes.pizzaSize);
     }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    getPaymentOptions();
+  }, []);
 
   // ?. nullish -- pode não existir, mas evita a quebra da página. Apresenta um valor nulo, caso não exista
 
@@ -128,9 +108,7 @@ export default function Checkout() {
               <p>
                 {pizzaOrder?.item.name} - {pizzaOrder?.item.size}
               </p>
-              <p>
-                {convertToCurrency(pizzaOrder?.item.value)}
-              </p>
+              <p>{convertToCurrency(pizzaOrder?.item.value)}</p>
             </CheckoutItemFlex>
           </CheckoutItem>
           <CheckoutItem>
@@ -138,9 +116,16 @@ export default function Checkout() {
             <CheckoutItemFlex>
               <PaymentMethodGroup>
                 <label htmlFor="payments">Selecione a forma de pagamento</label>
-                <select name="payments" id="payments" defaultValue={""} onChange={handleChange}>
-                  <option disabled value="">Selecione</option>
-                    {paymentOptions.map(({ id, value, text}) => (
+                <select
+                  name="payments"
+                  id="payments"
+                  defaultValue={""}
+                  onChange={handleChange}
+                >
+                  <option disabled value="">
+                    Selecione
+                  </option>
+                  {paymentOptions.map(({ id, value, text }) => (
                     <option key={id} value={value}>
                       {text}
                     </option>
@@ -157,10 +142,12 @@ export default function Checkout() {
             </CheckoutItemFlex>
           </CheckoutItem>
           <CheckoutAction>
-            <Button onClick={doOrder} disabled={!Boolean(paymentType)}>Fazer pedido</Button>
+            <Button onClick={handleClick} disabled={!Boolean(paymentType)}>
+              Fazer pedido
+            </Button>
           </CheckoutAction>
         </>
       )}
     </Layout>
-  ) 
+  );
 }
--- src/pages/sizes/Sizes.tsx
+++ src/pages/sizes/Sizes.tsx
@@ -1,110 +1,105 @@
-import { useContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { routes } from "../../routes"
-import { Button } from "../../components/button/Button"
-import { Layout } from "../../components/layout/Layout"
-import { Title } from "../../components/title/Title"
-import { RadioCard, SizeActionWrapper, SizeContentWrapper } from "./Sizes.style"
-import OrderContext from "../../contexts/OrderContext"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../routes";
+import { Button } from "../../components/button/Button";
+import { Layout } from "../../components/layout/Layout";
+import { Title } from "../../components/title/Title";
+import {
+  RadioCard,
+  SizeActionWrapper,
+  SizeContentWrapper,
+} from "./Sizes.style";
+import OrderContext from "../../contexts/OrderContext";
 
 export default function Sizes() {
-  const navigate = useNavigate()
-  const { pizzaSize, setPizzaSize } = useContext(OrderContext)
+  const navigate = useNavigate();
+  const { pizzaSize, setPizzaSize } = useContext(OrderContext);
 
-  const sizeOptions = [
-    {
-      id: "10",
-      flavours: 1,
-      size: 35,
-      slices: 8,
-      text: "Grande",
-    },
-    {
-      id: "11",
-      flavours: 2,
-      size: 35,
-      slices: 8,
-      text: "Grande",
-    },
-    {
-      id: "20",
-      flavours: 1,
-      size: 28,
-      slices: 4,
-      text: "Média",
-    },
-    {
-      id: "21",
-      flavours: 2,
-      size: 28,
-      slices: 4,
-      text: "Média",
-    },
-    {
-      id: "30",
-      flavours: 1,
-      size: 18,
-      slices: 1,
-      text: "Broto",
-    },
-    {
-      id: "31",
-      flavours: 2,
-      size: 18,
-      slices: 1,
-      text: "Broto",
-    },
-  ]
+  const [sizeId, setSizeId] = useState("");
+  const [pizzaSizeOptions, setPizzaSizeOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [sizeId, setSizeId] = useState("")
+  const getPizzaSizeOptions = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:8000/pizza/sizes");
+      const options = await response.json();
+      setPizzaSizeOptions(options);
+    } catch (error) {
+      alert(`Deu ruim: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const getPizzaSize = (sizeId: string) => {
-    return sizeOptions.filter((option) => option.id === sizeId)
-  }
+    return pizzaSizeOptions.filter((option) => option.id === sizeId);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSizeId(event.target.value)
-  }
+    setSizeId(event.target.value);
+  };
 
   const handleBack = () => {
-    navigate(routes.home)
-  }
+    navigate(routes.home);
+  };
 
   const handleNext = () => {
-    const selectedSize = getPizzaSize(sizeId)
-    setPizzaSize(selectedSize)
-    const flavourSelect = selectedSize[0].flavours
-    flavourSelect === 2 ? navigate(routes.pizzaFlavourDual) : navigate(routes.pizzaFlavour)
-  }
+    const selectedSize = getPizzaSize(sizeId);
+    const selectedFlavour = selectedSize[0].flavours;
+    setPizzaSize(selectedSize);
+
+    if (selectedFlavour == 1) {
+      navigate(routes.pizzaFlavour);
+    } else if (selectedFlavour == 2) {
+      navigate(routes.pizzaFlavourDual);
+    }
+  };
 
   useEffect(() => {
-    if (!pizzaSize) return
-    setSizeId(pizzaSize[0].id)
-    
-  }, [])
-  
-  
+    if (!pizzaSize) return;
+    setSizeId(pizzaSize[0].id);
+  }, []);
+
+  useEffect(() => {
+    getPizzaSizeOptions();
+  }, []);
 
   return (
     <Layout>
       <Title tabIndex={0}>Escolha o tamanho da sua pizza</Title>
-      <SizeContentWrapper>
-        {sizeOptions.map(({id, size, slices, flavours, text}) => (
-          <RadioCard key={id}>
-            <input type="radio" id={id} name="sizes" onChange={handleChange} value={id} checked={sizeId === id}/>
-            <label htmlFor={id}>
-              {text} - {flavours} sabores
-              <span>
-                Pizza com {slices} pedaços e {size}cm
-              </span>
-            </label>
-          </RadioCard>
-        ))}
-      </SizeContentWrapper>
+      {isLoading ? (
+        <SizeContentWrapper>
+          <Title>Carregando...</Title>
+        </SizeContentWrapper>
+      ) : (
+        <SizeContentWrapper>
+          {pizzaSizeOptions.map(({ id, size, slices, flavours, text }) => (
+            <RadioCard key={id}>
+              <input
+                type="radio"
+                id={id}
+                name="sizes"
+                onChange={handleChange}
+                value={id}
+                checked={sizeId === id}
+              />
+              <label htmlFor={id}>
+                {text} - {flavours} sabores
+                <span>
+                  Pizza com {slices} pedaços e {size}cm
+                </span>
+              </label>
+            </RadioCard>
+          ))}
+        </SizeContentWrapper>
+      )}
       <SizeActionWrapper>
-        <Button inverse="inverse" onClick={handleBack}>Voltar</Button>
+        <Button inverse="inverse" onClick={handleBack}>
+          Voltar
+        </Button>
         <Button onClick={handleNext}>Escolha o Sabor</Button>
       </SizeActionWrapper>
     </Layout>
-  )
+  );
 }
